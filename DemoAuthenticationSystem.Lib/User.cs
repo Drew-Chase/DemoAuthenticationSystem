3ef7da260f4e17adb1839c8e8e9d29ec13ca9c33
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-03 15:09:18 -0400

    Add User struct to Authentication System
    A new User struct was added to the DemoAuthenticationSystem.Lib namespace. This struct contains necessary properties for a user in an authentication system, like Id, Username, Email and Password. This provides the basic user functionality in the authentication system.

---
+++ DemoAuthenticationSystem.Lib/User.cs
@@ -0,0 +1,31 @@
+﻿namespace DemoAuthenticationSystem.Lib;
+
+/// <summary>
+/// Represents a user in the authentication system.
+/// </summary>
+public struct User()
+{
+    /// <summary>
+    /// Gets or sets the Id of the User.
+    /// </summary>
+    /// <remarks>
+    /// The Id property represents the unique identifier of a User in the system. It is an integer value that is automatically generated when a User is created.
+    /// </remarks>
+    public int Id { get; internal set; }
+
+    /// <summary>
+    /// Gets or sets the username of the user.
+    /// </summary>
+    /// <value>The username.</value>
+    public string Username { get; set; }
+
+    /// <summary>
+    /// Represents the email address of a user.
+    /// </summary>
+    public string Email { get; set; }
+
+    /// <summary>
+    /// Represents the password of a user.
+    /// </summary>
+    public string Password { get; set; }
+}
