6e4b830d99e8bf4e4784fdab7d39c30997bad8a0
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-03 16:15:15 -0400

    Introduce hash-based user ID and implement user authentication
    The UserManager class has been updated to implement a hash-based user ID system. This allows for better security as user IDs are now encoded and decoded via Hashids. This update also includes the implementation of user authentication, which verifies usernames and passwords, and a token generation system to authenticate sessions.

--- DemoAuthenticationSystem.Lib/UserManager.cs
+++ DemoAuthenticationSystem.Lib/UserManager.cs
@@ -1,6 +1,9 @@
 ﻿using System.Data.SQLite;
 using System.Diagnostics.CodeAnalysis;
+using System.Net;
 using Chase.CommonLib.Math;
+using HashidsNet;
+using Newtonsoft.Json;
 
 namespace DemoAuthenticationSystem.Lib;
 
@@ -27,6 +30,11 @@ public sealed class UserManager : IDisposable
     /// The file path to the SQLite database used for storing user information.
     /// </summary>
     private const string DatabaseFile = "users.db";
+
+    /// <summary>
+    /// Represents the Hashids instance used for encoding and decoding user IDs.
+    /// </summary>
+    private readonly Hashids _hashids = new("DemoAuthenticationSystem", 8);
 
     public UserManager()
     {
@@ -74,16 +82,21 @@ public sealed class UserManager : IDisposable
     /// </summary>
     /// <param name="id">The ID of the user.</param>
     /// <returns>The user object with the specified ID, or null if the user is not found.</returns>
-    public User? GetUserById(int id)
+    public User? GetUserById(string id)
     {
+        // Decode the hash ID to get the actual user ID.
+        int[]? decodedUserIds = _hashids.Decode(id);
+        if (decodedUserIds is null || decodedUserIds.Length == 0) return null;
+        int userId = decodedUserIds[0];
+
         using var command = new SQLiteCommand("SELECT * FROM Users WHERE Id = @Id", _connection);
-        command.Parameters.AddWithValue("@Id", id);
+        command.Parameters.AddWithValue("@Id", userId);
         using var reader = command.ExecuteReader();
         if (reader.Read())
         {
             return new User()
             {
-                Id = reader.GetInt32(0),
+                Id = _hashids.Encode(reader.GetInt32(0)),
                 Username = reader.GetString(1),
                 Email = reader.GetString(2),
                 Password = reader.GetString(3)
@@ -99,7 +112,41 @@ public sealed class UserManager : IDisposable
     /// <param name="id">The ID of the user to retrieve.</param>
     /// <param name="user">The retrieved user. Null if the user doesn't exist.</param>
     /// <returns>True if the user exists, false otherwise.</returns>
-    public bool TryGetUserById(int id, [NotNullWhen(true)] out User? user) => (user = GetUserById(id)) != null;
+    public bool TryGetUserById(string id, [NotNullWhen(true)] out User? user) => (user = GetUserById(id)) != null;
+
+    public bool AuthenticateUser(string username, string password)
+    {
+        using var command = new SQLiteCommand("SELECT * FROM Users WHERE Username = @Username", _connection);
+        command.Parameters.AddWithValue("@Username", username);
+        using var reader = command.ExecuteReader();
+        if (reader.Read())
+        {
+            var encryption = new Crypt();
+            return encryption.Decrypt(reader.GetString(3)) == password;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Generates a token for the specified user and unique key.
+    /// </summary>
+    /// <param name="user">The user for whom the token is generated.</param>
+    /// <param name="uniqueKey">The unique key used for generating the token.</param>
+    /// <returns>The generated token as a string.</returns>
+    public string GenerateToken(User user, string uniqueKey)
+    {
+        var encryption = new Crypt();
+        return encryption.Encrypt(JsonConvert.SerializeObject(new { user.Id, user.Username, user.Password, uniqueKey }));
+    }
+
+    /// <summary>
+    /// Generates a token for the specified user and unique key.
+    /// </summary>
+    /// <param name="user">The user for whom the token is generated.</param>
+    /// <param name="address">The connecting IP Address</param>
+    /// <returns>The generated token as a string.</returns>
+    public string GenerateToken(User user, IPAddress address) => GenerateToken(user, address.ToString());
 
     public void Dispose()
     {
