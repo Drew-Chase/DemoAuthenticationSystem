65320f403179f8139744a668837cd21c6f22471f
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-04 08:18:59 -0400

    Add Empty property and IsEmpty check to User class
    Added an Empty static property to the User class in the Authentication library, which is used to represent an absence of a valid user instance. Also, implemented the IsEmpty property to check if the User object is empty or not, determined by whether the Id property is null or an empty string.

--- DemoAuthenticationSystem.Lib/User.cs
+++ DemoAuthenticationSystem.Lib/User.cs
@@ -28,4 +28,27 @@ public struct User()
     /// Represents the password of a user.
     /// </summary>
     public string Password { get; set; }
+
+    /// <summary>
+    /// Represents an empty User object.
+    /// </summary>
+    /// <remarks>
+    /// The Empty property is a static property that represents an empty User object. It can be used to indicate the absence of a valid User instance.
+    /// </remarks>
+    /// <value>
+    /// An empty User object.
+    /// </value>
+    public static User Empty => new User();
+
+    /// <summary>
+    /// Gets a value indicating whether the User is empty or not.
+    /// </summary>
+    /// <value>
+    /// <c>true</c> if the User is empty; otherwise, <c>false</c>.
+    /// </value>
+    /// <remarks>
+    /// The IsEmpty property is a boolean value that indicates whether the User object is empty or not.
+    /// A User is considered empty when the Id property is null or an empty string.
+    /// </remarks>
+    public bool IsEmpty => string.IsNullOrEmpty(Id);
 }
